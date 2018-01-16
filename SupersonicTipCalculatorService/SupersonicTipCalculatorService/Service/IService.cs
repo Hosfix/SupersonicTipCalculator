ee7cc34d715f516fc6a12f4acdff109cdc9c5975
Hosfix-DELL <hosfix@gmail.com>
2018-01-16 16:56:25 +0100

    Estructura de la rama

---
+++ SupersonicTipCalculatorService/SupersonicTipCalculatorService/Service/IService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace SupersonicTipCalculatorService.Service
+{
+    [ServiceContract]
+    public interface IService
+    {
+        [OperationContract]
+        string GetData(int value);
+    }
+}
