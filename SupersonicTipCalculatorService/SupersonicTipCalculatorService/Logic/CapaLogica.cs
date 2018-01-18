0ca7a656aad44b54e6d4304ecfc66d42f89b8854
Hosfix-DELL <hosfix@gmail.com>
2018-01-18 09:29:45 +0100

    Optimización de los metodos

--- SupersonicTipCalculatorService/SupersonicTipCalculatorService/Logic/CapaLogica.cs
+++ SupersonicTipCalculatorService/SupersonicTipCalculatorService/Logic/CapaLogica.cs
@@ -39,11 +39,11 @@ namespace SupersonicTipCalculatorService.Logic
         private static decimal GetTip(List<RateEntity> ratesList, OrderEntity order, string currency)
         {
             var amount = order.Amount;
-            var camino = new List<RateEntity>();
+            var betterWay = new List<RateEntity>();
             if (order.Currency != currency)
-                camino = GetBetterWay(ratesList, order, currency);
+                betterWay = FindPossibleChangeRecursive(order.Currency, currency, ratesList).OrderBy(r => r.Count).First();
 
-            foreach (var rate in camino)
+            foreach (var rate in betterWay)
             {
                 amount = amount * rate.Rate;
             }
@@ -51,30 +51,25 @@ namespace SupersonicTipCalculatorService.Logic
             return amount * 0.05M;
         }
 
-        private static List<RateEntity> GetBetterWay(List<RateEntity> ratesList, OrderEntity order, string currency)
-        {
-            return Rates(order.Currency, currency, ratesList).OrderBy(r => r.Count).First();
-        }
-
-        public static List<List<RateEntity>> Rates(string baseCode, string targetCode, List<RateEntity> toSee)
+        private static List<List<RateEntity>> FindPossibleChangeRecursive(string from, string to, List<RateEntity> ratesList)
         {
             var results = new List<List<RateEntity>>();
 
-            List<RateEntity> possible = toSee.FindAll(r => r.From == baseCode);
-            List<RateEntity> hits = possible.FindAll(p => p.To == targetCode);
-            if (hits.Count > 0)
+            List<RateEntity> listWithFrom = ratesList.FindAll(f => f.From == from);
+            List<RateEntity> listWithFromAndTo = listWithFrom.FindAll(ft => ft.To == to);
+            if (listWithFromAndTo.Count > 0)
             {
-                possible.RemoveAll(hits.Contains);
-                results.AddRange(hits.Select(hit => new List<RateEntity> { hit }));
+                listWithFrom.RemoveAll(listWithFromAndTo.Contains);
+                results.AddRange(listWithFromAndTo.Select(lft => new List<RateEntity> { lft }));
             }
 
-            List<RateEntity> newToSee = toSee.FindAll(item => !possible.Contains(item));
-            foreach (RateEntity posRate in possible)
+            List<RateEntity> newListToSee = ratesList.FindAll(s => !listWithFrom.Contains(s));
+            foreach (RateEntity possibleRate in listWithFrom)
             {
-                List<List<RateEntity>> otherConversions = Rates(posRate.To, targetCode, newToSee);
-                RateEntity rate = posRate;
-                otherConversions.ForEach(result => result.Insert(0, rate));
-                results.AddRange(otherConversions);
+                List<List<RateEntity>> conversions = FindPossibleChangeRecursive(possibleRate.To, to, newListToSee);
+                RateEntity rate = possibleRate;
+                conversions.ForEach(result => result.Insert(0, rate));
+                results.AddRange(conversions);
             }
             return results;
         }
